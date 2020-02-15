e2c85156add33016134ca4e19cfcd2a913fdf13e
roberto.ferro <roberto.ferro@ikdev.eu>
2020-02-15 16:07:44 +0100

    Implemented custom services

--- service/service_container.go
+++ service/service_container.go
@@ -8,8 +8,9 @@ import (
 // Here is where service container is built.
 // As you can see the service container is provided by Uber DIG library.
 // Se its documentation (https://godoc.org/go.uber.org/dig) to implement extra services.
-func BuildContainer(controllers []interface{}, middleware interface{}) *dig.Container {
+func BuildContainer(controllers []interface{}, middleware interface{}, services []interface{}) *dig.Container {
 	container := dig.New()
+	bindServices(services)
 
 	gwf.Controllers = controllers
 	gwf.Middleware = middleware
@@ -23,6 +24,13 @@ func BuildContainer(controllers []interface{}, middleware interface{}) *dig.Cont
 	gwf.Container = container
 
 	return container
+}
+
+// Bind custom services with defaults
+func bindServices(services []interface{}) {
+	for _, s := range services {
+		Services = append(Services, s)
+	}
 }
 
 var Services = []interface{}{
