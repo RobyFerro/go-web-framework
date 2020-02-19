1cb962ea7ad8eba9ee6f480dcaed91428b578ab6
Roberto Ferro <roberto.ferro@verticalbooking.com>
2020-02-19 17:24:08 +0100

    updated jwt auth flow

--- jwt_auth.go
+++ jwt_auth.go
@@ -10,51 +10,44 @@ import (
 
 // Auth structure will be used to handle the authenticated user data.
 type Auth struct {
-	User struct {
-		ID       uint
-		Name     string
-		Surname  string
-		Username string
-		Password string
-	} `json:"user"`
-	Key string
+	ID       uint 
+	Name     string
+	Surname  string
+	Username string
+	Password string
 }
 
 // GetUser will parse incoming request and returns the user data.
-func (c *Auth) GetUser(req *http.Request) error {
+func (c *Auth) GetUser(req *http.Request, key string) error {
 	bearerSchema := "Bearer "
 	tokenString := req.Header.Get("Authorization")
 
 	claims := jwt.MapClaims{}
 	_, _ = jwt.ParseWithClaims(tokenString[len(bearerSchema):], claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(c.Key), nil
+		return []byte(key), nil
 	})
 
-	for key, val := range claims {
-		if key == "user" {
-			userData := val.(string)
-			err := json.Unmarshal([]byte(userData), &c.User)
+	userData := claims["user"].(string)
+	err := json.Unmarshal([]byte(userData), &c)
 
-			if err != nil {
-				return err
-			}
-		}
+	if err != nil {
+		return err
 	}
 
 	return nil
 }
 
 // NewToken will return a new JWT token
-func (c *Auth) NewToken() (string, bool) {
-	c.User.Password = ""
-	userDataString, _ := json.Marshal(c.User)
+func (c *Auth) NewToken(key string) (string, bool) {
+	c.Password = ""
+	userDataString, _ := json.Marshal(c)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user": string(userDataString),
 		"exp":  time.Now().Add(time.Hour * time.Duration(2)).Unix(),
 		"iat":  time.Now().Unix(),
 	})
 
-	tokenString, err := token.SignedString([]byte(c.Key))
+	tokenString, err := token.SignedString([]byte(key))
 
 	if err != nil {
 		return "", false
@@ -64,16 +57,16 @@ func (c *Auth) NewToken() (string, bool) {
 }
 
 // RefreshToken will grefresh the a speficic token
-func (c *Auth) RefreshToken(req http.ResponseWriter) bool {
+func (c *Auth) RefreshToken(req http.ResponseWriter, key string) bool {
 	expirationTime := time.Now().Add(5 * time.Minute)
-	userDataString, _ := json.Marshal(c.User)
+	userDataString, _ := json.Marshal(c)
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"user": string(userDataString),
 		"exp":  expirationTime,
 		"iat":  time.Now().Unix(),
 	})
 
-	tokenString, err := token.SignedString([]byte(c.Key))
+	tokenString, err := token.SignedString([]byte(key))
 	req.Header().Set("refresh-token", tokenString)
 
 	if err != nil {
