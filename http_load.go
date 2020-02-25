206d3c3447733a586a12cf5c141e4ad3f5dc08ad
Roberto Ferro <roberto.ferro@verticalbooking.com>
2020-02-25 10:41:44 +0100

    Fix target URL

--- http_load.go
+++ http_load.go
@@ -55,7 +55,7 @@ func (c *HttpLoad) Run(conf *Conf) {
 func attack(r LoadRoute, url string) {
 	rate := vegeta.Rate{Freq: r.Rate, Per: time.Second}
 	duration := 5 * time.Second
-	targetUrl := fmt.Sprintf("http://%s/%s", url, r.Url)
+	targetUrl := fmt.Sprintf("http://%s%s", url, r.Url)
 	fmt.Printf("Testing: %s\n", targetUrl)
 
 	target := vegeta.NewStaticTargeter(vegeta.Target{
