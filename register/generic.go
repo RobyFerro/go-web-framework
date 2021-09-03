43776dbcfdf28fae7323f6b5b5a049b637c882e6
Roberto Ferro <roberto.ferro@ikdev.it>
2021-09-03 15:23:49 +0200

    Organized types in multiple files

---
+++ register/generic.go
@@ -0,0 +1,13 @@
+package register
+
+// ServiceRegister defines a controller register type.
+// This will be used to resolve this register in service container
+type ServiceRegister []interface{}
+
+// ModelRegister defines a controller register type.
+// This will be used to resolve this register in service container
+type ModelRegister []interface{}
+
+// ControllerRegister defines a controller register type.
+// This will be used to resolve this register in service container
+type ControllerRegister []interface{}
